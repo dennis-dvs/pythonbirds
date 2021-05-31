81db3ed708aae38d32068b72a90c414ee9a4e709
Dennis <dennis.vieira1@gmail.com>
2021-05-31 00:01:23 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -17,7 +19,16 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filhos in luciano.filhos:
         print(filhos.nome)
-    luciano.sobrenome = 'Ramalho' # Não é uma boa prática fazer em tempo de execução
-    del luciano.filhos            # Tanto criação e exclusão devido a dificuldade de entender o código
-    print(luciano.__dict__)
-    print(renzo.__dict__)
+    luciano.sobrenome = 'Ramalho'  # Não é uma boa prática fazer em tempo de execução
+    del luciano.filhos  # Tanto criação e exclusão devido a dificuldade de entender o código
+    luciano.olhos = 1
+    del luciano.olhos # Aqui será apagado o atributo do objeto mais o valor da classe retornará a aparecer
+    print(luciano.__dict__)  # __dict__ Mantem apenas referencias para os atributos de instâncias...
+    print(renzo.__dict__)  # ...não para atributos de classes por ex.: olhos. Se forem alterados ele aparece
+    # print(Pessoa.nome)   # Neste caso não tem sentido já que tem várias classes referenciadas não pelo atributo
+    # mais sim pelo seu conteúdo
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
